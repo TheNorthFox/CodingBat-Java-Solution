df7bbe8652a32f052699c3e0949d5edb09ba9909
Min K <yegofm@yahoo.com>
2017-01-30 19:43:26 -0800

    Update Warmup-2.java

--- Warmup-2.java
+++ Warmup-2.java
@@ -13,3 +13,39 @@ public String stringTimes(String str, int n) {
   }
   return s;
 }
+/*
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+frontTimes("Chocolate", 2) → "ChoCho"
+frontTimes("Chocolate", 3) → "ChoChoCho"
+frontTimes("Abc", 3) → "AbcAbcAbc"
+
+*/
+public String frontTimes(String str, int n) {
+  String s = "";
+  int length = 3;
+  if(length > str.length())
+  length = str.length();
+  for(int i=0; i<n; i++){
+    s += str.substring(0,length);
+  }
+  return s;
+  
+}
+/*
+Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+countXX("abcxx") → 1
+countXX("xxx") → 2
+countXX("xxxx") → 3
+*/
+public int countXX(String str) {
+  int count =0;
+  for(int i=0; i< str.length()-1; i++ ){
+    if(str.substring(i,i+2).equals("xx"))
+    count++;
+  }
+  return count;
+}
+
+
